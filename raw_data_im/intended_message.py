86c277c3763250e59a8cec54ce30eafcf48a5398
Mike van Lenthe <mikevanlenthe@msn.com>
2018-04-11 02:34:07 +0200

    program updated

--- raw_data_im/intended_message.py
+++ raw_data_im/intended_message.py
@@ -4,7 +4,7 @@ import io
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import *
-
+import nltk
 
 def find_files(directory):
     paths = []
@@ -26,6 +26,7 @@ def collect_intended_message(read_path):
     c_stemmed = Counter()  # count tokens with stemming filter
     c_lemmatized_stopwords = Counter()  # count tokens lemmatized and stop words filtered out
     c_lemmatized_stopwords_custom = Counter()  # count tokens lemmatized, stop words filtered out + custom filter
+    c_lemmatized_stopwords_pos_tagged = Counter()  # count tokens lemmatized, stop words filtered out + pos_tags
 
     # custom word filter
     custom_filter = ['--', '-', '—', 'trump:', '...']
@@ -48,6 +49,7 @@ def collect_intended_message(read_path):
     # scrape tokens from articles in txt format
     for txt_file in txt_files:
         txt_tokens = []
+        verbs_in_article = []
         infile = open(txt_file, 'r',  errors='ignore')
         for line in infile:
             tokens = line.split()
@@ -76,12 +78,31 @@ def collect_intended_message(read_path):
             lemmatized = lemmatizer.lemmatize(token)
             c_lemmatized_stopwords[lemmatized] += 1
 
-        # lemmatize, stopwords and custom filter
+        # custom filter
         custom_filtered = [word for word in txt_tokens if word not in custom_filter]
+
+        # lemmatize, stopwords and custom filter
         custom_and_stopwords_filtered = [word for word in custom_filtered if word not in Stopwords]
         for token in custom_and_stopwords_filtered:
             lemmatized = lemmatizer.lemmatize(token)
             c_lemmatized_stopwords_custom[lemmatized] += 1
+
+        # pos tagging
+        pos_tags = nltk.pos_tag(txt_tokens)
+        for pos_tag in pos_tags:
+            tag = pos_tag[1]
+            token = pos_tag[0]
+            if tag == "VB":
+                verbs_in_article.append(token)  # collect verbs
+
+        # lemmatize, stopwords and pos tagged VERBS
+        pos_tag_and_stopwords_filtered = [word for word in verbs_in_article if word not in Stopwords]
+        for token in pos_tag_and_stopwords_filtered:
+            lemmatized = lemmatizer.lemmatize(token)
+            c_lemmatized_stopwords_pos_tagged[lemmatized] += 1
+
+    #print(verbs_in_article)
+    #print(text)
 
     print("NOTE: All tokens converted to lowercase!")
     print()
@@ -96,7 +117,8 @@ def collect_intended_message(read_path):
     print("Lemmatization and stopwords filter token count: \n", c_lemmatized_stopwords.most_common(50))
     print()
     print("Lemmatization, stopwords filter, custom filter token count: \n", c_lemmatized_stopwords_custom.most_common(50))
-
+    print()
+    print("Lemmatization, stopwords filter, custom filter token count: \n", c_lemmatized_stopwords_pos_tagged.most_common(50))
 
 def main():
     collect_intended_message("./")
